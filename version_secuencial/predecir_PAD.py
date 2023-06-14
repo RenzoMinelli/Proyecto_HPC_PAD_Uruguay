997260731be3945d71f6479caac62766c8a0cea5
EjMendez <ej.mendez2904@gmail.com>
2023-06-13 22:08:05 -0300

    aut main predecir_pad

--- version_secuencial/predecir_PAD.py
+++ version_secuencial/predecir_PAD.py
@@ -1,7 +1,10 @@
+import generar_matrices_bloque
+import entrenar_modelos_por_bloque
+import predecir_por_bloque
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
     
-   # generar_matrices_por_bloque()
-  #  entrenar_modelos_por_bloque()
-  #  predecir_por_bloque()
+   generar_matrices_por_bloque()
+   entrenar_modelos_por_bloque()
+   predecir_por_bloque()

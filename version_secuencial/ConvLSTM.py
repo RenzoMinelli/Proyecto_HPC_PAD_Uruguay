f73ebac08ec40da505adeab35bcf1b0242bad3df
Renzo Minelli <renminelli@gmail.com>
2023-06-18 21:09:11 -0300

    refactor: fix a modelo

--- version_secuencial/ConvLSTM.py
+++ version_secuencial/ConvLSTM.py
@@ -11,13 +11,11 @@ def create_model(input_shape=(None, 3, 3, 1)):
     model.add(BatchNormalization())
 
     model.add(ConvLSTM2D(filters=80, kernel_size=(3, 3),
-               padding='same', return_sequences=True))
+               padding='same', return_sequences=False))  
     model.add(BatchNormalization())
 
-    # Añade una capa Flatten para convertir la salida multidimensional de las capas ConvLSTM2D en un vector 1D.
     model.add(Flatten())
 
-    # Añade una capa Dense con 1 neurona y función de activación lineal para predecir un solo valor flotante.
     model.add(Dense(1, activation='linear'))
 
     model.compile(loss='mse', optimizer=Adam(learning_rate=0.001))

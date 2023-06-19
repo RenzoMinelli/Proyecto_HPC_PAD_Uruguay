094988a5c5c869390052598d904f6b3db0fe0857
Renzo Minelli <renminelli@gmail.com>
2023-06-19 19:44:47 -0300

    refactor: rename

--- version_paralela/predecir_por_bloque.py
+++ version_paralela/predecir_por_bloque.py
@@ -50,7 +50,7 @@ def procesar_modelos(archivos_modelos, cola_res):
         prediccion = modelo.predict(bloque)[0,0]
         cola_res.put((num_modelo, prediccion))
 
-def predecir_por_bloque_paralelo(steps=5):
+def predecir_por_bloque(steps=5):
     archivos_modelos = [f for f in os.listdir(directorio_modelos) if f.endswith('.h5')]
     
     cantidad_modelos = len(archivos_modelos)
@@ -125,4 +125,4 @@ def predecir_por_bloque_paralelo(steps=5):
             df.to_csv(ruta_completa, header=None, index=False)
 
 if __name__ == "__main__":
-    predecir_por_bloque_paralelo()
+    predecir_por_bloque()

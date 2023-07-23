1d595793aefdc75b803b4ba08ed6db2bcb48a1d6
Renzo Minelli <renminelli@gmail.com>
2023-07-23 18:29:25 -0300

    feat: agregar evaluacion de modelos

---
+++ version_c/version_paralela/scripts_python/entrenar_modelos_por_bloque_evaluacion.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import os
+
+import sys
+
+from CustomModel import create_model
+from config import *
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+directorio_modelos = DIRECTORIO_MODELOS_GENERADOS
+
+def save_model(model, filename):
+    model.save(filename)
+
+def procesar_archivos(archivo, steps_para_evaluacion):
+
+    print('Procesando archivo: ' + archivo)
+    ruta_completa = os.path.join(DIRECTORIO_CSVS_MATRICES_GENERADAS, archivo)
+    df = pd.read_csv(ruta_completa, header=None)
+    clave = archivo[:-4]
+
+    cols = list(df)[1:] # las columnas que vamos a usar para entrenamineto es todo menos la primera
+    df_for_training = df[cols].astype(float)
+    # sacamos las ultimas steps_para_evaluacion filas
+    df_for_training = df_for_training[:-steps_para_evaluacion]
+
+    n_future = 1 # cuantos pasos al futuro predecir 
+    n_past = 14 # cuantos pasos al pasado mirar
+    df_for_training = df_for_training.values
+
+    #Empty lists to be populated using formatted training data
+    trainX = []
+    trainY = []
+
+    n_future = 1   # Number of days we want to look into the future based on the past days.
+    n_past = 14  # Number of past days we want to use to predict the future.
+
+    pos_a_predecir = 4
+    for i in range(n_past, len(df_for_training) - n_future +1):
+        trainX.append(df_for_training[i - n_past:i, 0:df_for_training.shape[1]])
+        trainY.append(df_for_training[i + n_future - 1:i + n_future, pos_a_predecir])
+
+    trainX, trainY = np.array(trainX), np.array(trainY)
+
+    model = create_model(trainX, trainY)
+
+    # fit the model
+    history = model.fit(trainX, trainY, epochs=5, batch_size=16, validation_split=0.1, verbose=1)
+
+    nombre_archivo = clave + '.keras'
+    save_model(model, directorio_modelos + nombre_archivo) 
+
+if __name__ == "__main__":
+    archivo = sys.argv[1]
+    steps_para_evaluacion = int(sys.argv[2])
+    procesar_archivos(archivo, steps_para_evaluacion)
---
+++ version_c/version_paralela/scripts_python/evaluar_predicciones.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import os
+from config import *
+import sys 
+import matplotlib.pyplot as plt
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def evaluar_predicciones(steps_a_evaluar):
+
+    matrices_valores_reales = {}
+    directorio_matrices_reales = DIRECTORIO_CSVS_MATRICES_POR_FECHA_ANTERIORES
+
+    archivos = sorted(os.listdir(directorio_matrices_reales))[-steps_a_evaluar:]
+    indice = 0
+    for archivo in archivos:
+        ruta_completa = os.path.join(directorio_matrices_reales, archivo)
+        df = pd.read_csv(ruta_completa, header=None)
+        matrices_valores_reales[indice] = df
+        indice += 1
+
+    direccion_matrices_predicciones = DIRECTORIO_MATRICES_PREDICCIONES
+    matrices_predicciones = {}
+    archivos = sorted(os.listdir(direccion_matrices_predicciones))[-steps_a_evaluar:]
+    indice = 0
+    for archivo in archivos:
+        ruta_completa = os.path.join(direccion_matrices_predicciones, archivo)
+        df = pd.read_csv(ruta_completa, header=None)
+        matrices_predicciones[indice] = df
+        indice += 1
+
+    claves = sorted(matrices_valores_reales.keys())
+    promedios = []
+
+    for clave in claves:
+        matriz_real = matrices_valores_reales[clave]
+        matriz_prediccion = matrices_predicciones[clave]
+        diferencia = matriz_real.values - matriz_prediccion.values
+        suma = np.sum(diferencia)
+        suma = abs(suma)
+        promedio = suma / CANTIDAD_MEDIDORES
+        promedios.append(promedio)
+
+    # Tracing the averages
+    plt.plot(promedios)
+    plt.xlabel('Steps')
+    plt.ylabel('Average difference between predictions and real values')
+    plt.show()
+        
+if __name__ == "__main__":
+    steps_a_evaluar = int(sys.argv[1])
+    evaluar_predicciones(steps_a_evaluar)
